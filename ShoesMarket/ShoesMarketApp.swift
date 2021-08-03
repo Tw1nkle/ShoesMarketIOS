1bdc99294744babd40af7b1eb9f543fc63c4a84d
Daria Fediashova <fediashova.it@gmail.com>
2021-08-03 16:34:49 +0300

    Initial Commit

---
+++ ShoesMarket/ShoesMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoesMarketApp.swift
+//  ShoesMarket
+//
+//  Created by Дарья Федяшова on 03.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShoesMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
